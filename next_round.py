ed333f18db33eef89c60b00292b44f94bcdd23b7
mszahan <mszahan1@gmail.com>
2024-07-12 00:08:59 +0600

    solved 158A - Next Round

---
+++ next_round.py
@@ -0,0 +1,15 @@
+n_k = input().split()
+scores = input().split()
+
+k = int(n_k[1])
+count = 0
+position_score = int(scores[k-1])
+
+for score in scores:
+    if int(score) >= position_score and int(score) !=0:
+        count +=1
+    else:
+        count = count + 0
+
+print(count)
+
